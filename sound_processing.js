3a0a1b1af084cf3fcf473878c8d445b372dd9e7f
Ahmed Mahboub <ahmed.mahboub99@gmail.com>
2020-08-24 12:29:24 +0200

    Update sound_processing.js

--- sound_processing.js
+++ sound_processing.js
@@ -1,77 +1,70 @@
 var recorder, soundFile, mic, pause_flag, amplitute, one_pause;
 var list_pause = new Array();
 
-recorder = new p5.SoundRecorder();
-soundFile = new p5.SoundFile();
-mic = new p5.AudioIn();
-// users must manually enable their browser microphone for recording to work properly!
-mic.start();
-console.log(mic.getLevel);
+// function setup() {
+//   // # Setting up the recording configurations
+//   recorder = new p5.SoundRecorder();
+//   soundFile = new p5.SoundFile();
 
-function setup() {
-  // # Setting up the recording configurations
-  recorder = new p5.SoundRecorder();
-  soundFile = new p5.SoundFile();
+//   // create an audio in
+//   mic = new p5.AudioIn();
 
-  // create an audio in
-  mic = new p5.AudioIn();
+//   // users must manually enable their browser microphone for recording to work properly!
+//   mic.start();
 
-  // users must manually enable their browser microphone for recording to work properly!
-  mic.start();
+//   // create a sound recorder
+//   //recorder = new p5.SoundRecorder();
 
-  // create a sound recorder
-  //recorder = new p5.SoundRecorder();
+//   // connect the mic to the recorder
+//   recorder.setInput(mic);
 
-  // connect the mic to the recorder
-  recorder.setInput(mic);
+//   // recorder.stop();
+//   // soundFile.play();
+//   // saveSound(soundFile, 'mySound.wav');
 
-  // recorder.stop();
-  // soundFile.play();
-  // saveSound(soundFile, 'mySound.wav');
+//   // shared variable for the thead
+//   pause_flag = 0;
+//   amplitute = 0;
+//   recorder.record(soundFile);
+// }
 
-  // shared variable for the thead
-  pause_flag = 0;
-  amplitute = 0;
-  recorder.record(soundFile);
-}
+// function foo() {
+//   //Threading
 
-function foo() {
-  //Threading
+//   console.log("\n\nfoo running in the thread\n");
+//   console.log("Flag = ", pause_flag);
+//   amplitute = mic.getLevel() ** 2 * 10000;
+//   console.log(amplitute);
 
-  console.log("\n\nfoo running in the thread\n");
-  console.log("Flag = ", pause_flag);
-  amplitute = mic.getLevel() ** 2 * 10000;
-  console.log(amplitute);
+//   if (amplitute < 1) {
+//     //threshold value =  1
+//     pause_flag += 1;
+//     console.log("[Pause] amplitute < 0 ", amplitute);
+//   } else {
+//     pause_flag = 0;
+//     console.log("[Sound] ampliude > 0 ", amplitute);
+//   }
 
-  if (amplitute < 1) {
-    //threshold value =  1
-    pause_flag += 1;
-    console.log("[Pause] amplitute < 0 ", amplitute);
-  } else {
-    pause_flag = 0;
-    console.log("[Sound] ampliude > 0 ", amplitute);
-  }
+//   if (pause_flag == 31) {
+//     // confirming a complete pause
+//     // *
+//     stopThread(foo);
 
-  if (pause_flag == 31) {
-    // confirming a complete pause
-    // *
-    stopThread(foo);
+//     recorder.stop();
+//     console.log("stoping the recorder");
+//     one_pause = soundFile.getBlob();
+//     console.log(one_pause);
 
-    recorder.stop();
-    console.log("stoping the recorder");
-    one_pause = soundFile.getBlob();
-    console.log(one_pause);
+//     // * appending list_pause
+//     list_pause.push(one_pause);
+//   }
+// }
 
-    // * appending list_pause
-    list_pause.push(one_pause);
-  }
-}
-
-function stopThread() {
-  clearInterval(thread_interval);
-  //TODO: start new setup
-  setup();
-  //TODO: call set interval
-  thread_interval = setInterval(foo, 0.1 * 1000);
-}
+// function stopThread() {
+//   clearInterval(thread_interval);
+//   //TODO: start new setup
+//   setup();
+//   //TODO: call set interval
+//   thread_interval = setInterval(foo, 0.1 * 1000);
+// }
 //var thread_interval = setInterval(foo, 0.1 * 1000);
